85e8e7caa79f463a33a96399a48a08adee1f9529
Rick Winfrey <rick.winfrey@gmail.com>
2014-01-13 14:09:31 -0600

    Adds basic anagram functionality

--- spec/anagramer/core_spec.clj
+++ spec/anagramer/core_spec.clj
@@ -2,6 +2,51 @@
   (:require [speclj.core :refer :all]
             [anagramer.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(defmacro with-redef-word-list [& body]
+  `(with-redefs [words (clojure.string/split (slurp "test_words") #"\n")]
+     ~@body))
+
+(describe "anagramize"
+  (it "returns a vector of anagrams for a given string"
+    (with-redef-word-list
+      (should== ["ate" "eat"] 
+        (anagramize "tea")))))
+
+(describe "letter-count"
+  (it "returns 0 for empty string"
+    (should= 0
+             (letter-count "")))
+
+  (it "returns 1 for 'a'"
+    (should= 1
+             (letter-count "a")))
+
+  (it "does not count spaces between letters"
+    (should= 3
+             (letter-count "a bc"))))
+
+(describe "reduced-by-count"
+  (it "reduces word list by letter count"
+    (with-redef-word-list
+      (should== ["ate" "eat" "tea" "cat" "hat"]
+                (reduced-by-count 3))))
+  
+  (it "returns empty list when word list contains no words matching given letter count"
+    (with-redef-word-list
+      (should= [] 
+               (reduced-by-count 4)))))
+
+(describe "anagram-match?"
+  (it "returns true if an input string and word contain exactly the same letters"
+    (should= true
+             (anagram-match? "hat" "tah")))
+  
+  (it "returns false if an input string and word do not contain exactly the same lettesr"
+    (should= false
+             (anagram-match? "hat" "cat"))))
+
+(describe "reduced-by-letter-match"
+  (it "reduces word list by matching letters of input vs word in word list"
+    (with-redef-word-list
+      (should== ["ate" "eat"]
+                (reduced-by-letter-match  words "tea")))))
--- src/anagramer/core.clj
+++ src/anagramer/core.clj
@@ -1,1 +1,29 @@
 (ns anagramer.core)
+
+(def words (clojure.string/split (slurp "words") #"\n"))
+
+(defn letter-count [input]
+  (count (clojure.string/replace input #"\ " "")))
+
+(defn reduced-by-count [let-count]
+  (reduce (fn [out-list word]
+            (if (= let-count (count word))
+              (conj out-list word)
+              out-list)) [] words))
+
+(defn anagram-match? [word input]
+  (loop [remaining-input input]
+    (cond
+      (= word input) false
+      (empty? (rest remaining-input)) true
+      (>= (.indexOf word (str (first remaining-input))) 0) (recur (rest remaining-input))
+      :else false)))
+
+(defn reduced-by-letter-match [reduced-words input]
+  (reduce (fn [matched-words word]
+            (if (anagram-match? word input)
+              (conj matched-words word)
+              matched-words)) [] reduced-words))
+
+(defn anagramize [input]
+  (reduced-by-letter-match (reduced-by-count (letter-count input)) input))
